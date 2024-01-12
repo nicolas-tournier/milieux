6c2ae2f18f6080b9356a38e26ed20df0d1ee22ed
nicolas-tournier <tourns@hotmail.com>
2024-01-13 10:30:55 +1100

    try some new colours

--- src/const/constants.ts
+++ src/const/constants.ts
@@ -1,9 +1,27 @@
+// export const colorRange = [
+//   [255, 0, 0, 255],     // Very upset - Red
+//   [255, 128, 0, 255],    // Upset - Orange Red
+//   [255, 160, 90, 255],   // Slightly upset - Orange
+//   [255, 255, 115, 255],   // Neutral - Yellow
+//   [0, 255, 85, 255],    // Very happy - Turquoise
+//   [0, 170, 255, 255],   // Very excited - Blue
+//   [0, 85, 255, 255]     // Extremely excited - Dark Blue
+// ];
+
+// export const colorRange = [
+//   [255, 0, 255, 255],   // Magenta
+//   [255, 165, 0, 255],   // Orange
+//   [0, 255, 0, 255],     // Acid Green
+//   [0, 200, 200, 255],   // Peacock Blue
+//   [128, 0, 32, 255]     // Burgundy
+// ];
+
 export const colorRange = [
   [255, 0, 0, 255],     // Very upset - Red
-  [255, 128, 0, 255],    // Upset - Orange Red
-  [255, 160, 90 , 255],   // Slightly upset - Orange
-  [255, 255, 115, 255],   // Neutral - Yellow
-  [0, 255, 85, 255],    // Very happy - Turquoise
+  [128, 0, 32, 255],    // Burgundy
+  [255, 0, 255, 255],   // Magenta
+  [255, 165, 0, 255],   // Orange
+  [0, 255, 0, 255],     // Acid Green
   [0, 170, 255, 255],   // Very excited - Blue
-  [0, 85, 255, 255]     // Extremely excited - Dark Blue
+  [0, 200, 200, 255],   // Peacock Blue
 ];
